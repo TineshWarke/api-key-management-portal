23bb2ee175d11256b863bf1553376b2e96221e0e
TineshWarke <tineshwarke2000@gmail.com>
2025-11-01 23:43:46 +0530

    Changes

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const baseURL = import.meta.env.VITE_API_URL || "https://api-key-management-portal.vercel.app/";
 
 const api = axios.create({
   baseURL,
